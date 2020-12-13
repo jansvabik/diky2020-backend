5389c2e336ed5aeaee56fe8bc9c8253ffce776e0
Jan Švábík <jansvabik@jansvabik.cz>
2020-12-13 19:33:53 +0100

    refactor: separate Thanks and Donation structs

--- internal/thanks/thanks.go
+++ internal/thanks/thanks.go
@@ -8,16 +8,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// Donation is a specific donation data structure
+type Donation struct {
+	Target string `json:"target" bson:"target"`
+	Amount uint   `json:"amount" bson:"amount"`
+}
+
 // Thanks is a thanks data structure
 type Thanks struct {
 	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name     string             `json:"name" bson:"name"`
 	Text     string             `json:"text" bson:"text"`
 	Time     *time.Time         `json:"time" bson:"time"`
-	Donation struct {
-		Target string `json:"target" bson:"target"`
-		Amount uint   `json:"amount" bson:"amount"`
-	} `json:"donation,omitempty" bson:"donation"`
+	Donation *Donation          `json:"donation,omitempty" bson:"donation"`
 }
 
 // collection returns the collection for the current data
