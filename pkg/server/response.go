2df52bd8db0def002b11802db17ea82b85c97cdd
Jan Švábík <jansvabik@jansvabik.cz>
2020-12-13 19:55:48 +0100

    feat: add error 500 function to call

--- pkg/server/response.go
+++ pkg/server/response.go
@@ -1,8 +1,22 @@
 package server
 
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
 // APIResponse is a http api response JSON structure
 type APIResponse struct {
 	Status string      `json:"status"`
 	Msg    string      `json:"msg"`
 	Data   interface{} `json:"data,omitempty"`
+}
+
+// APIInternalServerError returns the fiber function for
+// internal server error including the JSON data
+func APIInternalServerError(c *fiber.Ctx) error {
+	return c.Status(500).JSON(APIResponse{
+		Status: "ERR",
+		Msg:    "Internal server error.",
+		Data:   nil,
+	})
 }
