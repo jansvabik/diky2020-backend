a3103aa933bb9f6a07b481b4384d1645c52b10db
Jan Švábík <jansvabik@jansvabik.cz>
2020-12-15 11:36:26 +0100

    refactor: change PATCH to POST for Donio webhook

--- pkg/router/init.go
+++ pkg/router/init.go
@@ -17,6 +17,6 @@ func InitRoutes() {
 	router.Get("/thanks", thanks.ReadHandler)
 	router.Get("/thanks/:page", thanks.ReadHandler)
 	router.Post("/thanks", thanks.CreateHandler)
-	router.Patch("/thanks/:id/donated", thanks.DonatedHandler)
+	router.Post("/thanks/:id/donation", thanks.DonatedHandler)
 	router.Listen(":" + strconv.Itoa(app.State.Cfg.Net.Port))
 }
